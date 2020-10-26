11613eb2a25a909eddf4cc67c09e47581a26ac56
Michael Bamford <michael@michaelbamford.com>
2020-10-26 11:42:25 +1100

    Add User scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # resources :sessions, only: [:new, :create, :destroy]
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'cookies', to: 'cookies#index'
   resources :authors, only: [ :index, :show, :new, :create ]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_005927) do
+ActiveRecord::Schema.define(version: 2020_10_26_001558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,14 @@ ActiveRecord::Schema.define(version: 2020_10_20_005927) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["author_id"], name: "index_recipes_on_author_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "pages", "cookbooks"
